2def895ab0700a6f2aa685d91f98d71f3daa9e02
LesteR <lesterlaw7798@gmail.com>
2016-03-05 01:18:34 +0800

    '9/3'

--- bookbook/settings.py
+++ bookbook/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'jo478()2z&&*@0f2t!!b8^pypy2thqh2#y6gbz0fqoi7h!)fgz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
